9d97bc6680279bb9b197ef70af2a4219ab9892fb
Manav-1200 <manav.neupane1@gmail.com>
2026-09-08 20:08:14 +0545

    feat(memory): store active_workspace and project in working memory

--- mavis_core/src/memory/working.rs
+++ mavis_core/src/memory/working.rs
@@ -3,7 +3,7 @@
 
 #![allow(dead_code)]
 
-use crate::context_snapshot::{BrowserTab, WindowInfo};
+use crate::context_snapshot::{BrowserTab, ProjectInfo, WindowInfo};
 use crate::models::event::Event;
 use serde::{Deserialize, Serialize};
 use std::collections::VecDeque;
@@ -19,6 +19,10 @@ pub struct WorkingMemory {
     pub active_window: Option<WindowInfo>,
     #[serde(default)]
     pub open_windows: Vec<WindowInfo>,
+    #[serde(default)]
+    pub active_workspace: Option<u64>,
+    #[serde(default)]
+    pub project: Option<ProjectInfo>,
     pub last_clipboard: Option<String>,
     pub context_timestamp: Option<u64>,
     pub user_name: Option<String>,
