fa6781b4b19c6921874ad964c3e268b938826a81
Manav-1200 <manav.neupane1@gmail.com>
2026-09-08 20:07:56 +0545

    chore(platform): update Windows stub for new open_windows signature

--- mavis_core/src/platform/windows.rs
+++ mavis_core/src/platform/windows.rs
@@ -18,7 +18,7 @@ impl WindowTracker for WindowsWindowTracker {
     fn active_window(&self) -> Result<(String, String, u32), PlatformError> {
         Err(PlatformError("Windows window tracking not yet implemented".into()))
     }
-    fn open_windows(&self) -> Result<Vec<(String, String, u32)>, PlatformError> {
+    fn open_windows(&self) -> Result<Vec<WindowInfo>, PlatformError> {
         Err(PlatformError("Windows window tracking not yet implemented".into()))
     }
     fn subscribe_changes(&self) -> Result<tokio::sync::mpsc::Receiver<WindowEvent>, PlatformError> {
